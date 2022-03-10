f9f72ba79c91b8b8dec76cd71af7cc6b6dbf8840
meetwq <meetwq@gmail.com>
2022-03-10 23:22:09 +0800

    format

--- nonebot_plugin_petpet/functions.py
+++ nonebot_plugin_petpet/functions.py
@@ -432,7 +432,9 @@ async def roll(users: List[UserInfo], **kwargs) -> BytesIO:
     return save_gif(frames, 0.1)
 
 
-async def play_game(users: List[UserInfo], args: List[str] = [], **kwargs) -> BytesIO:
+async def play_game(
+    users: List[UserInfo], args: List[str] = [], **kwargs
+) -> Union[str, BytesIO]:
     img = users[0].img
     bg = await load_image("play_game/0.png")
     text = args[0] if args else "来玩休闲游戏啊"
@@ -721,22 +723,19 @@ async def perfect(users: List[UserInfo], **kwargs) -> BytesIO:
     return save_jpg(frame)
 
 
-async def subscribe(
+async def follow(
     users: List[UserInfo], args: List[str] = [], **kwargs
 ) -> Union[str, BytesIO]:
     img = users[0].img
     img = resize(circle(img), (200, 200))
 
-    fontname = "SourceHanSansSC-Regular.otf"
-    font = await load_font(fontname, 60)
-
+    font = await load_font(DEFAULT_FONT, 60)
     ta = "女同" if users[0].gender == "female" else "男同"
     name = (args[0] if args else "") or users[0].name or ta
     text_name = name
     text_name_w, text_name_h = font.getsize(text_name)
-    text_subscribe = "关注了你"
-    text_subscribe_w, text_subscribe_h = font.getsize(text_subscribe)
-    text_width = max(text_name_w, text_subscribe_w)
+    text_follow = "关注了你"
+    text_width = max(text_name_w, font.getsize(text_follow)[0])
     if text_width >= 1000:
         return "名字太长了哦，改短点再试吧~"
 
@@ -744,8 +743,8 @@ async def subscribe(
     frame.paste(img, (50, 50), mask=img)
     text_frame = Image.new("RGBA", (text_width + 50, 300), (255, 255, 255, 0))
     draw = ImageDraw.Draw(text_frame)
-    draw.text((0, 135 - text_name_h), text_name, font=font,fill="black")
-    draw.text((0, 145), text_subscribe, font=font, fill="grey")
+    draw.text((0, 135 - text_name_h), text_name, font=font, fill="black")
+    draw.text((0, 145), text_follow, font=font, fill="grey")
     frame.paste(text_frame, (300, 0), mask=text_frame)
 
     return save_jpg(frame)
