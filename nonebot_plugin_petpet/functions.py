81769023cfa307fa851f30be558f4cb8b9fe1e80
lclaochou <100130244+lclaochou@users.noreply.github.com>
2022-05-11 21:21:40 +0800

    Resized the image

--- nonebot_plugin_petpet/functions.py
+++ nonebot_plugin_petpet/functions.py
@@ -1398,6 +1398,6 @@ async def thinkwhat(users: List[UserInfo], **kwargs) -> BytesIO:
     img = to_jpg(img).convert("RGBA")
     bg = await load_image("thinkwhat/0.png")
     frame = Image.new("RGBA", bg.size, (255, 255, 255, 0))
-    frame.paste(fit_size(img, (437, 315)), (569, 90))
+    frame.paste(fit_size(img, (534, 493)), (530, 0))
     frame.paste(bg, mask=bg)
     return save_jpg(frame)
