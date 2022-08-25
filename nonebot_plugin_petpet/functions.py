ad45e0438a4f1feedea0150c56e616d03bd347d5
meetwq <meetwq@gmail.com>
2022-08-25 22:13:36 +0800

    adjust read_book

--- nonebot_plugin_petpet/functions.py
+++ nonebot_plugin_petpet/functions.py
@@ -1450,19 +1450,34 @@ def blood_pressure(img: BuildImage = UserImg(), arg=NoArg()):
 def read_book(img: BuildImage = UserImg(), arg: str = Arg()):
     frame = load_image("read_book/0.png")
     points = ((0, 108), (1092, 0), (1023, 1134), (29, 1134))
-    cover = img.resize((1000, 1100), keep_ratio=True).perspective(points)
+    img = img.resize((1000, 1100), keep_ratio=True, direction="north")
+    cover = img.perspective(points)
     frame.paste(cover, (1138, 1172), below=True)
     if arg:
-        try:
-            frame.draw_text(
-                (870, 1500, 1110, 2280),
-                arg,
-                max_fontsize=200,
-                min_fontsize=70,
-                allow_wrap=True,
-                fill="white",
-                weight="bold",
+        chars = list(" ".join(arg.splitlines()))
+        pieces: List[BuildImage] = []
+        for char in chars:
+            piece = BuildImage(
+                Text2Image.from_text(char, 200, fill="white", weight="bold").to_image()
             )
-        except:
+            if re.fullmatch(r"[a-zA-Z0-9\s]", char):
+                piece = piece.rotate(-90, expand=True)
+            else:
+                piece = piece.resize_canvas((piece.width, piece.height - 40), "south")
+            pieces.append(piece)
+        w = max((piece.width for piece in pieces))
+        h = sum((piece.height for piece in pieces))
+        if w > 240 or h > 3000:
             return TEXT_TOO_LONG
+        text_img = BuildImage.new("RGBA", (w, h))
+        h = 0
+        for piece in pieces:
+            text_img.paste(piece, ((w - piece.width) // 2, h), alpha=True)
+            h += piece.height
+        if h > 780:
+            ratio = 780 / h
+            text_img = text_img.resize((int(w * ratio), int(h * ratio)))
+        text_img = text_img.rotate(3, expand=True)
+        w, h = text_img.size
+        frame.paste(text_img, (870 + (240 - w) // 2, 1500 + (780 - h) // 2), alpha=True)
     return frame.save_jpg()
